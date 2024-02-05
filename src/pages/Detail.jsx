dbcfa5d377491899d2d5b54f4bce584f36fa6540
Hyeryeon-Seo <0168968060@naver.com>
2024-02-05 11:47:20 +0900

    style(주석삭제) : detail페이지 삭제버튼 잘 작동, 불필요한 코드및주석삭제

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -16,7 +16,6 @@ function Detail({ commentList, setCommentList }) {
 	};
 
 	// 삭제버튼
-	// 둘다 set해줘야 잘 적용됨 -> 이렇게 하는게 맞나
 	const deleteCommentHandler = (id) => {
 		if (window.confirm("정말 삭제하실 건가요?")) {
 			setCommentList((prevCommentList) =>
@@ -25,13 +24,6 @@ function Detail({ commentList, setCommentList }) {
 			alert("팬레터가 삭제되었습니다");
 			// 홈화면으로 이동
 			navigate("/");
-
-			setFilteredByMemList((prevCommentList) =>
-				prevCommentList.filter((comment) => comment.id !== id)
-			); // 엇 메인에서와다르게 이거없어도 삭제 잘 먹히는거같은데?
-			// 아래에서 화면이동하면서 자연스럽게 리렌더링?되어서인듯!
-			//(Home에서는, setCommentList만해줄경우 바로화면에서 삭제안되고 남아있었다가 다른거 클릭하고 넘어와야 사라졌음)
-			// BUT 컴포넌트분리후에 그런건지, Home페이지에서 삭제안되어있음 & 홈화면이동을 setFiltered..위에 써줘야 작동하게되었다 (바뀜)
 		}
 	};
 
