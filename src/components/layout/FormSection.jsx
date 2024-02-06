6e13131332b2f9fa18b8ea2720307403a3d2706c
Hyeryeon-Seo <0168968060@naver.com>
2024-02-06 18:43:17 +0900

    이미지 주소 경로 재설정

--- src/components/layout/FormSection.jsx
+++ src/components/layout/FormSection.jsx
@@ -7,7 +7,7 @@ function FormSection({ memberList, setCommentList }) {
 		<S.FormSection>
 			<h2>Write & Send</h2>
 			<S.ImgFormBox>
-				<S.ArtistImg src="src/assets/aespaGroupImage.jpg" />
+				<S.ArtistImg src="public\img\aespaGroupImage.jpg" />
 				{/*form태그 - 코멘트(팬레터) 입력, 등록하기*/}
 				<CommentForm
 					memberList={memberList}
