a2572817b068c801091462c3c74ff8489aebc813
Hyeryeon-Seo <0168968060@naver.com>
2024-02-06 18:56:07 +0900

    이미지 확장자변경

--- src/components/layout/FormSection.jsx
+++ src/components/layout/FormSection.jsx
@@ -7,10 +7,7 @@ function FormSection({ memberList, setCommentList }) {
 		<S.FormSection>
 			<h2>Write & Send</h2>
 			<S.ImgFormBox>
-				<S.ArtistImg
-					src="/assets/images/aespaGroupImage.jpg"
-					alt="aespa-image"
-				/>
+				<S.ArtistImg src="src/assets/aespaGroupImage.png" alt="aespa-image" />
 				{/*form태그 - 코멘트(팬레터) 입력, 등록하기*/}
 				<CommentForm
 					memberList={memberList}
